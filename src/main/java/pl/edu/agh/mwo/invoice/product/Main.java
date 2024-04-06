b3c71a6187ae9a23914caeedf9372c1c9d79a0ab
asiaKaplanska <kaplanska.joanna@gmail.com>
2024-04-06 21:17:39 +0200

    Set pot price as constant

--- src/main/java/pl/edu/agh/mwo/invoice/product/Main.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Main.java
@@ -6,10 +6,11 @@ import pl.edu.agh.mwo.invoice.Invoice;
 import java.math.BigDecimal;
 
 public class Main {
-    private static int potPrice = 10;
+    private static final int POT_PRICE = 10;
+    
     public static void main(String[] args) {
         Invoice invoice = new Invoice(new DefaultPrinter());
-        invoice.addProduct(new OtherProduct("Doniczka", new BigDecimal(potPrice)), 2);
+        invoice.addProduct(new OtherProduct("Doniczka", new BigDecimal(POT_PRICE)), 2);
         System.out.println(invoice.printInvoice());
     }
 }
