629ad32b5e1915dd55d6f296d5bb398124ea036e
asiaKaplanska <kaplanska.joanna@gmail.com>
2024-04-06 21:15:09 +0200

    Fix magic number

--- src/main/java/pl/edu/agh/mwo/invoice/product/Main.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Main.java
@@ -6,8 +6,8 @@ import pl.edu.agh.mwo.invoice.Invoice;
 import java.math.BigDecimal;
 
 public class Main {
+    private static int potPrice = 10;
     public static void main(String[] args) {
-        var potPrice = 10;
         Invoice invoice = new Invoice(new DefaultPrinter());
         invoice.addProduct(new OtherProduct("Doniczka", new BigDecimal(potPrice)), 2);
         System.out.println(invoice.printInvoice());
